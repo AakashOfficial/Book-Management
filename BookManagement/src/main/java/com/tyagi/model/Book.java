9f66bbffe734374ecca7c2ad563dcfc836d5a9a2
Aakash Tyagi <aakashtyagi1996@outlook.com>
2017-11-21 21:45:14 +0530

    Few Changes In Configuration !

---
+++ BookManagement/src/main/java/com/tyagi/model/Book.java
@@ -0,0 +1,38 @@
+package com.tyagi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Book {
+
+	private int bookId;
+	private String title;
+	private double price;
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
---
+++ BookManagement/src/main/java/com/tyagi/model/Issue.java
@@ -0,0 +1,5 @@
+package com.tyagi.model;
+
+public class Issue {
+
+}
